8d96eea24a46e226f4e05f155febbd3e6bf1a588
Xiangjiang Wang <54554363+XiangjiangWang@users.noreply.github.com>
2023-04-23 18:53:23 -0500

    Update and rename analysis.py to find-soft-clip.py

--- find-soft-clip.py
+++ find-soft-clip.py
@@ -33,10 +33,9 @@ def revcomp(seq):
 TH = 0.3 # relative length of soft clipped sequence
 
 samfile1 = pysam.AlignmentFile('final.sorted.bam', 'rb')
-samfile2 = pysam.AlignmentFile('candidates.bam', 'wb', template=samfile1)
+samfile2 = pysam.AlignmentFile('soft-clip.sam', 'w', template=samfile1)
 
-fa = '' # fasta
-sc = [] # soft clipped sequences
+qname = {} # names of read pairs to output
 for read in tqdm(samfile1.fetch(until_eof=True)):
     if read.is_unmapped:
         continue
@@ -45,22 +44,51 @@ for read in tqdm(samfile1.fetch(until_eof=True)):
     astart = read.query_alignment_start
     astop = read.query_alignment_end
 
+    # cautious when both reads have soft clipped sequences
     if astart > TH*qlen:
         # left soft
-        samfile2.write(read)
         s = read.query_sequence[:astart]
-        sc.append(revcomp(s))
-        fa += f'>{read.query_name} left soft\n{s} {read.query_sequence[astart:]}\n'
+        fa = f'>{read.query_name} left soft\n{s} {read.query_sequence[astart:]}\n' # add a space between human and plasmid
+        qname[read.query_name] = [None, None, revcomp(s), fa] # r1, r2, soft clipped sequence, fasta record
     elif astop < (1-TH)*qlen:
         # right soft
-        samfile2.write(read)
         s = read.query_sequence[astop:]
-        sc.append(s)
-        fa += f'>{read.query_name} right soft\n{read.query_sequence[:astop]} {s}\n'
+        fa = f'>{read.query_name} right soft\n{read.query_sequence[:astop]} {s}\n' # add a space between human and plasmid
+        qname[read.query_name] = [None, None, s, fa] # r1, r2, soft clipped sequence, fasta record
 
-sc = '\n'.join(sorted(sc))
+# sort read pairs with soft clipped sequences
+qname = {k: v for k, v in sorted(qname.items(), key=lambda x: x[1][2])}
 
-with open('candidates.fasta', 'w') as f:
-    f.write(fa)
+# fetch read pairs in qname
+samfile1.reset()
+for read in tqdm(samfile1.fetch(until_eof=True)):
+    try:
+        v = qname[read.query_name]
+        if read.is_read1:
+            v[0] = read
+        elif read.is_read2:
+            v[1] = read
+    except KeyError:
+        continue
+samfile1.close()
+
+# write read pairs
+all_sc = ''
+all_fa = ''
+for n, (r1, r2, sc, fa) in qname.items():
+    if r1 is not None:
+        samfile2.write(r1)
+    else:
+        print(f'r1 does not exist: {n}')
+    if r2 is not None:
+        samfile2.write(r2)
+    else:
+        print(f'r2 does not exist: {n}')
+    all_sc += sc+'\n'
+    all_fa += fa
+samfile2.close()
+
+with open('soft-clip.fasta', 'w') as f:
+    f.write(all_fa)
 with open('soft-clip.txt', 'w') as f:
-    f.write(sc)
+    f.write(all_sc)
